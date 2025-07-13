473e49867a4afe391572766efda68bbe5a606c5b
unknown <kevinvasquez720@gmail.com>
2025-07-13 01:49:49 -0600

    ultima version 07/13

--- JS/ejercicio1.js
+++ JS/ejercicio1.js
@@ -3,4 +3,5 @@
 function mayorDeEdad (edad){
     let resultado = edad >= 18 ? "la persona es mayor de edad" : "la persona no es mayor de edad";
     console.log('Resultado:', resultado);
+    return resultado;
 }
--- JS/ejercicio2.js
+++ JS/ejercicio2.js
@@ -4,6 +4,18 @@ Examen =20%
 • asistencia = 10% 
 • investigación = 30%*/
 function promedio(tareas, examen, asistencia, investigacion){
-    let resultado = (tareas * 0.4) + (examen * 0.2) + (asistencia * 0.1) + (investigacion * 0.3);
-    console.log('El promedio es:', resultado);
+    if (typeof tareas !== 'number' || typeof examen !== 'number' || typeof asistencia !== 'number' || typeof investigacion !== 'number') {
+        console.log("Por favor, ingrese valores numéricos válidos.");
+        return;
+    }
+    else if (tareas < 0 || examen < 0 || asistencia < 0 || investigacion < 0) {
+        console.log("Los valores no pueden ser negativos.");
+    }
+    else if (tareas > 10 || examen > 10 || asistencia > 10 || investigacion > 10) {
+        console.log("Los valores no pueden ser mayores a 10.");
+    }
+    else{
+        let resultado = (tareas * 0.4) + (examen * 0.2) + (asistencia * 0.1) + (investigacion * 0.3);
+        console.log('El promedio es:', resultado);
+    }
 }
